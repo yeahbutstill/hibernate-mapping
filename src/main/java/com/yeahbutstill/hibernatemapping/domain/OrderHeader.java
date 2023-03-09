b8cddd6f59eb9a8016d03a9f65b7f5a40679c5a7
Dani Setiawan <mayadani28@gmail.com>
2023-03-09 20:31:26 +0700

    Add files via upload

---
+++ src/main/java/com/yeahbutstill/hibernatemapping/domain/OrderHeader.java
@@ -0,0 +1,57 @@
+package com.yeahbutstill.hibernatemapping.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class OrderHeader {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        OrderHeader that = (OrderHeader) o;
+
+        if (!Objects.equals(id, that.id)) {
+            return false;
+        }
+        return Objects.equals(customer, that.customer);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (customer != null ? customer.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/test/java/com/yeahbutstill/hibernatemapping/repositories/OrderHeaderRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.yeahbutstill.hibernatemapping.repositories;
+
+import com.yeahbutstill.hibernatemapping.domain.OrderHeader;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+class OrderHeaderRepositoryTest {
+
+    @Autowired
+    OrderHeaderRepository orderHeaderRepository;
+
+    @Test
+    void testSaveOrder() {
+        OrderHeader orderHeader = new OrderHeader();
+        orderHeader.setCustomer("New Customer");
+        OrderHeader savedOrder = orderHeaderRepository.save(orderHeader);
+
+        assertNotNull(savedOrder);
+        assertNotNull(savedOrder.getId());
+
+        OrderHeader fetchedOrder = orderHeaderRepository.getReferenceById(savedOrder.getId());
+
+        assertNotNull(fetchedOrder);
+        assertNotNull(fetchedOrder.getId());
+    }
+}
