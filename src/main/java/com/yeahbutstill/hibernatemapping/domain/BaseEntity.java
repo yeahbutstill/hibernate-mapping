101630ac4c9c6b6315d8a246257c167e7198aa76
yeahbutstill <mayadani28@gmail.com>
2023-03-10 11:34:49 +0700

    mapping super class

---
+++ src/main/java/com/yeahbutstill/hibernatemapping/domain/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.yeahbutstill.hibernatemapping.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof BaseEntity that)) {
+            return false;
+        }
+
+        return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
--- src/main/java/com/yeahbutstill/hibernatemapping/domain/OrderHeader.java
+++ src/main/java/com/yeahbutstill/hibernatemapping/domain/OrderHeader.java
@@ -1,27 +1,11 @@
 package com.yeahbutstill.hibernatemapping.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-import java.util.Objects;
 
 @Entity
-public class OrderHeader {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class OrderHeader extends BaseEntity {
 
     private String customer;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getCustomer() {
         return customer;
@@ -36,22 +20,20 @@ public class OrderHeader {
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (!(o instanceof OrderHeader that)) {
+            return false;
+        }
+        if (!super.equals(o)) {
             return false;
         }
 
-        OrderHeader that = (OrderHeader) o;
-
-        if (!Objects.equals(id, that.id)) {
-            return false;
-        }
-        return Objects.equals(customer, that.customer);
+        return getCustomer() != null ? getCustomer().equals(that.getCustomer()) : that.getCustomer() == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (customer != null ? customer.hashCode() : 0);
+        int result = super.hashCode();
+        result = 31 * result + (getCustomer() != null ? getCustomer().hashCode() : 0);
         return result;
     }
 }
